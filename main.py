075c1316e1e9aad293d438f1672c6bc893c91fb1
cricehomelab <charles.e.riceiii@gmail.com>
2022-12-11 12:40:54 -0500

    Creating a README.md

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-"""
-Create an API that functions as a TODO list.
-I am doing this mostly from just the Fastapi documentation and what I can google around.
-This is not based off of a specific tutorial.
-I want to be able perform all CRUD functions with the API.
 
-TODO list for this app.
-1. Add items to the TODO list.                          => DONE
-2. Query a list of active items in the TODO list.       => IN PROGRESS
-    A. Currently able to query all TODO items.          => DONE
-    B. Able to query by TODO items of a specific status => DONE
-3. Update items in the TODO list.
-    A. add detail to the item
-    B. mark an item complete
-4. Create a list of recently completed items.
-5. Generate a productivity report.
-6. Send a reminder of items to add to report.
-7. Need to take an attempt at security.
-    A. Do this last.
-"""
 
 from fastapi import FastAPI
 from typing import Union
